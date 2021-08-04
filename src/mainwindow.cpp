5923b4beb89fa3d02a455d6d618fc0900575de07
redtide <redtid3@gmail.com>
2021-08-04 18:20:16 +0200

    Copyright headers update

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2020 Andrea Zanellato <redtid3@gmail.com>
+    Copyright (C) 2020-2021 Andrea Zanellato <redtid3@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
