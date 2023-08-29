aa4d87f3b26d7fd68b8f7c5412233b5493fc49bd
JungHee Lee <36289819+MarongHappy@users.noreply.github.com>
2023-08-29 13:16:13 +0900

    Create magnifiqus_ko.ts

---
+++ resources/translations/magnifiqus_ko.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>Qtilities::DialogAbout</name>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="27"/>
+        <source>Information</source>
+        <translation>정보</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="62"/>
+        <source>qrc:/about.html</source>
+        <translation>qrc:/about.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="81"/>
+        <source>Thanks</source>
+        <translation>감사한 분들</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="119"/>
+        <source>qrc:/thanks.html</source>
+        <translation>qrc:/thanks.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="138"/>
+        <source>License</source>
+        <translation>라이선스</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="176"/>
+        <source>qrc:/license.html</source>
+        <translation>qrc:/license.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="60"/>
+        <source>About</source>
+        <translation>정보</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="55"/>
+        <source>Author</source>
+        <translation>작성자</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::DialogPrefs</name>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="32"/>
+        <source>General</source>
+        <translation>일반</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="59"/>
+        <source>Background Color:</source>
+        <translation>배경 색상:</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="72"/>
+        <source>Border Color:</source>
+        <translation>테두리 색상:</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="79"/>
+        <source>Always on Top</source>
+        <translation>항상 위에</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="86"/>
+        <source>Autostart</source>
+        <translation>자동시작</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="113"/>
+        <source>Cursor</source>
+        <translation>커서</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="143"/>
+        <source>Size:</source>
+        <translation>크기:</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="163"/>
+        <source>px</source>
+        <translation>px</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="176"/>
+        <source>Show</source>
+        <translation>표시하기</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="150"/>
+        <source>Filled</source>
+        <translation>채워짐</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.cpp" line="41"/>
+        <source>Preferences</source>
+        <translation>환경설정</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::SystemTrayIcon</name>
+    <message>
+        <location filename="../../src/systemtrayicon.cpp" line="72"/>
+        <source>&amp;About</source>
+        <translation>정보(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="../../src/systemtrayicon.cpp" line="74"/>
+        <source>&amp;Preferences</source>
+        <translation>환경설정(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="../../src/systemtrayicon.cpp" line="76"/>
+        <source>&amp;Quit</source>
+        <translation>종료(&amp;Q)</translation>
+    </message>
+</context>
+</TS>
